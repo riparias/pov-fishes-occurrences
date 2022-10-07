6d2732933b115798d1656fbd5c7aed4cfb9c7e79
Damiano Oldoni <damiano.oldoni@inbo.be>
2022-10-07 16:51:32 +0200

    Remove basisOfRecord from event core
    Duplicate as it is in occurrence extensions as well.

--- sql/dwc_event.sql
+++ sql/dwc_event.sql
@@ -12,7 +12,6 @@ SELECT
   ''                                    AS datasetID,
   'POV'                                 AS institutionCode,
   'Targeted monitoring of fishes and crustacea by the Provincial Center of Environmental Research, Province East-Flanders, Belgium' AS datasetName,
-  'HumanObservation'                    AS basisOfRecord,
   o."SamplingProtocol"                  AS samplingProtocol, -- targeted monitoring
 -- EVENT
   o."ObservationIdentifier"             AS eventID,
