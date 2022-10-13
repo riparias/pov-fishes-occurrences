315c130b0b75d0223bd169c9842a517451f11d5e
Damiano Oldoni <damiano.oldoni@inbo.be>
2022-10-13 21:38:36 +0200

    Map LengthOfTime_Min in event core
    See #1

--- sql/dwc_event.sql
+++ sql/dwc_event.sql
@@ -13,6 +13,8 @@ SELECT
   'POV'                                 AS institutionCode,
   'Targeted monitoring of fishes and crustacea by the Provincial Center of Environmental Research, Province East-Flanders, Belgium' AS datasetName,
   o."SamplingProtocol"                  AS samplingProtocol, -- targeted monitoring
+  printf('%.2f', ROUND(o."LengthOfTime_Min", 2)) AS sampleSizeValue
+  'minute'                             AS sampleSizeUnit
 -- EVENT
   o."ObservationIdentifier"             AS eventID,
   date(o."DateOfObservation")           AS eventDate,
